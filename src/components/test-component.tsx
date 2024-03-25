31b1babceba153519254fedcc02ad77e6e342ed0
zadohu22 <zadohu22@gmail.com>
2024-03-25 13:25:16 -0500

    fix

--- src/components/test-component.tsx
+++ src/components/test-component.tsx
@@ -1,17 +1,4 @@
-import { FC } from 'react'
-
-interface IProps {
-    hello: string
-}
-
-/**
-* @author
-* @function @testComponent
-**/
-
-export const TestComponent:FC<IProps> = ({ hello }) => {
-    console.log('test');
-  return (
-    <div className='text-white'>{hello}</div>
-   )
- }
+export const TestComponent = () => {
+	console.log('test');
+	return <div className='text-white'>test</div>;
+};
