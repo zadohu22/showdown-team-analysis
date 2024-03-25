01079f6b849829cb43bd79eef2de7d9fa739ee53
zadohu22 <zadohu22@gmail.com>
2024-03-25 13:21:42 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import { Login, Signup } from './components/Auth';
 
 export default function App() {
 	return (
 		<Router>
 			<Routes>
 				<Route path='/' element={<Home />} />
-				<Route path='/login' element={<Login />} />
-				<Route path='/signup' element={<Signup />} />
 			</Routes>
 		</Router>
 	);
