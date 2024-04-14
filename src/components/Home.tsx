ae6a4472423236ca094fda467a0fdba0039fba02
zadohu22 <zadohu22@gmail.com>
2024-04-13 19:39:02 -0500

    begins separating individuals from whole team

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,7 @@
 import { convertToJson } from '../helpers/convertToJson';
 import { DisplayMons } from './DisplayMons';
+import { convertFullTeam } from '../helpers/convertFullTeam';
+import fullTeam from '../text/sample-from-showdown.txt';
 
 const Home = () => {
 	const vileplume = `\
@@ -61,6 +63,79 @@ const Home = () => {
 	- Foul Play  
 	- Taunt  
 	- Moonlight `;
+
+	const fullTeamText = `Vileplume @ Life Orb  
+	Ability: Effect Spore  
+	Level: 50  
+	Shiny: Yes  
+	Tera Type: Ground  
+	EVs: 140 HP / 116 Def / 252 SpA  
+	Modest Nature  
+	IVs: 0 Atk  
+	- Giga Drain  
+	- Moonblast  
+	- Sludge Bomb  
+	- Tera Blast  
+	
+	Gyarados @ Clear Amulet  
+	Ability: Intimidate  
+	Level: 50  
+	Tera Type: Steel  
+	EVs: 4 HP / 252 Atk / 252 Spe  
+	Adamant Nature  
+	- Waterfall  
+	- Protect  
+	- Iron Head  
+	- Roar  
+	
+	Umbreon @ Leftovers  
+	Ability: Inner Focus  
+	Level: 50  
+	Tera Type: Poison  
+	EVs: 252 HP / 156 Def / 100 SpD  
+	Bold Nature  
+	IVs: 0 Atk  
+	- Snarl  
+	- Foul Play  
+	- Taunt  
+	- Moonlight  
+	
+	Maushold-Four  
+	Ability: Friend Guard  
+	Level: 50  
+	Tera Type: Normal  
+	EVs: 252 HP  
+	IVs: 0 Atk  
+	- Follow Me  
+	- Super Fang  
+	- Encore  
+	- Helping Hand  
+	
+	Porygon2 @ Eviolite  
+	Ability: Download  
+	Level: 50  
+	Tera Type: Ghost  
+	EVs: 252 HP / 252 SpA  
+	Quiet Nature  
+	IVs: 0 Atk  
+	- Trick Room  
+	- Recover  
+	- Ice Beam  
+	- Tri Attack  
+	
+	Tinkaton @ Assault Vest  
+	Ability: Own Tempo  
+	Level: 50  
+	Tera Type: Fighting  
+	EVs: 252 HP / 252 Atk / 4 SpD  
+	Adamant Nature  
+	- Fake Out  
+	- Gigaton Hammer  
+	- Play Rough  
+	- Tera Blast 
+	`;
+
+	convertFullTeam(fullTeamText);
 
 	// const jsonData = convertToJson(text2);
 	// console.log(jsonData);
---
+++ src/helpers/convertFullTeam.ts
@@ -0,0 +1,8 @@
+export const convertFullTeam = (fullTeam: string) => {
+	const lines = fullTeam.split(/\n\s*\n/);
+
+	console.log(lines);
+	return lines;
+
+	// return console.log(fullTeam);
+};
